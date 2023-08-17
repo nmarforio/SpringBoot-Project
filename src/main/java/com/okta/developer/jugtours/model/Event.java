672a6d4ffb559e8604089de47be9be320431f5aa
python-game <n.marforio@gmail.com>
2023-08-17 11:19:42 +0200

    I used a exicting project to understand how to create a java/SpringBoot project next time bymyself, learning what is Lombok etc

---
+++ src/main/java/com/okta/developer/jugtours/model/Event.java
@@ -0,0 +1,32 @@
+package com.okta.developer.jugtours.model;
+
+// Lombok is an annotation processor that work on complie-time to add code into your class, usefull to avoid a lot of code like @Setter and @Getter or @Data
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.time.Instant;
+import java.util.Set;
+
+@Data // generate constructore with required arguments, ToString method and Hascode and equals implementation, check non-transient Field
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // Build complex object with the whatever value available
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Instant date;
+    private String title;
+    private String description;
+    @ManyToMany
+    private Set<User> attendees;
+}
